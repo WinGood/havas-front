1cbe8ac97200561c903b313578c666e0ee20afb6
zzzaki <techokill@gmail.com>
2017-08-19 17:34:50 +0800

    added new questions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-  currentPageIndex: number = 0;
+  currentPageIndex: number = 2;
   private currentQuestionIndex: number = 0;
   private questions = [{
     title: 'What age',
@@ -15,7 +15,7 @@ export class AppComponent {
     answers: [{
       portrait: 'assets/p1_1.jpg',
       selected: false,
-      desc: '20-35 years'
+      desc: 'under 20-35 years'
     }, {
       portrait: 'assets/p1_2.jpg',
       selected: false,
@@ -24,6 +24,134 @@ export class AppComponent {
       portrait: 'assets/p1_3.jpg',
       selected: false,
       desc: 'over 65 years'
+    }]
+  }, {
+    title: 'What is the mean HbA1c',
+    subtitle: 'of your diabetes patients?',
+    answers: [{
+      portrait: 'assets/p1_1.jpg',
+      selected: false,
+      desc: 'HbA1C <5.0%'
+    }, {
+      portrait: 'assets/p1_2.jpg',
+      selected: false,
+      desc: 'HbA1C 5.0% to 7%'
+    }, {
+      portrait: 'assets/p1_3.jpg',
+      selected: false,
+      desc: 'HbA1C >7%'
+    }]
+  }, {
+    title: 'What glucose control',
+    subtitle: 'do your diabetes patients exhibit?',
+    answers: [{
+      portrait: 'assets/p1_1.jpg',
+      selected: false,
+      desc: 'Stable blood glucose'
+    }, {
+      portrait: 'assets/p1_2.jpg',
+      selected: false,
+      desc: 'Fluctuating blood<br/>glucose'
+    }, {
+      portrait: 'assets/p1_3.jpg',
+      selected: false,
+      desc: 'High risk of<br/>hypoglycaemic episodes'
+    }]
+  }, {
+    title: 'What Body Mass Index',
+    subtitle: 'do your patients with type 2 diabetes have?',
+    answers: [{
+      portrait: 'assets/p1_1.jpg',
+      selected: false,
+      desc: 'BMI <25 kg/m2'
+    }, {
+      portrait: 'assets/p1_2.jpg',
+      selected: false,
+      desc: 'BMI 25–30 kg/m2'
+    }, {
+      portrait: 'assets/p1_3.jpg',
+      selected: false,
+      desc: 'BMI >30 kg/m2'
+    }]
+  }, {
+    title: 'What level of activity',
+    subtitle: 'do your patients with diabetes undertake?',
+    answers: [{
+      portrait: 'assets/p1_1.jpg',
+      selected: false,
+      desc: 'Little or no<br/>exercise'
+    }, {
+      portrait: 'assets/p1_2.jpg',
+      selected: false,
+      desc: 'Exercise 1–3<br/>times per week'
+    }, {
+      portrait: 'assets/p1_3.jpg',
+      selected: false,
+      desc: 'Exercise > 3<br/>times per week'
+    }]
+  }, {
+    title: 'What waist measurement',
+    subtitle: 'do your patients with diabetes have?',
+    answers: [{
+      portrait: 'assets/p1_1.jpg',
+      selected: false,
+      desc: '< 70 cm'
+    }, {
+      portrait: 'assets/p1_2.jpg',
+      selected: false,
+      desc: '70–100 cm'
+    }, {
+      portrait: 'assets/p1_3.jpg',
+      selected: false,
+      desc: '> 100 cm'
+    }]
+  }, {
+    title: 'For how long have your',
+    subtitle: 'patients been diagnosed with diabetes?',
+    answers: [{
+      portrait: 'assets/p1_1.jpg',
+      selected: false,
+      desc: '< 1 year'
+    }, {
+      portrait: 'assets/p1_2.jpg',
+      selected: false,
+      desc: '1–10 years'
+    }, {
+      portrait: 'assets/p1_3.jpg',
+      selected: false,
+      desc: '> 10 years'
+    }]
+  }, {
+    title: 'What Total Cholesterol levels',
+    subtitle: 'do your patients with diabetes exhibit?',
+    answers: [{
+      portrait: 'assets/p1_1.jpg',
+      selected: false,
+      desc: 'LDL <2.5 mmol/L'
+    }, {
+      portrait: 'assets/p1_2.jpg',
+      selected: false,
+      desc: 'LDL 2.5–3.0 mmol/L'
+    }, {
+      portrait: 'assets/p1_3.jpg',
+      selected: false,
+      desc: 'LDL >3.0 mmol/L'
+    }]
+  }, {
+    title: 'How many medicines',
+    subtitle: 'do your patients take to control their diabetes?',
+    answers: [{
+      portrait: 'assets/p1_1.jpg',
+      selected: false,
+      desc: 'None'
+    }, {
+      portrait: 'assets/p1_2.jpg',
+      selected: false,
+      desc: '1–2'
+    }, {
+      portrait: 'assets/p1_3.jpg',
+      selected: false,
+      desc: '≥ 3'
     }]
   }];
 
@@ -40,7 +168,18 @@ export class AppComponent {
   }
   
   get hasSelectedAnswers() {
-    return (this.question.answers.filter(question => question.selected)) ? true : false;
+    return this.question.answers.some(question => question.selected === true);
+  }
+  
+  nextQuestion() {
+    let currentIndex = this.currentQuestionIndex;
+    let hasNextQuestion = this.questions[currentIndex += 1];
+
+    if (hasNextQuestion) {
+      this.currentQuestionIndex += 1;
+    } else {
+      this.nextStep();
+    }
   }
 
   toggleSelected(index) {
